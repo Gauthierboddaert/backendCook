c8b8366eeaa9ad05a88ac47586ea5c092b316ce0
rayles59 <83212748+rayles59@users.noreply.github.com>
2022-12-05 14:28:20 +0100

    add OAuth

--- src/Controller/api/RecetteController.php
+++ src/Controller/api/RecetteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\api;
 
 use App\Repository\RecetteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\Annotation\Groups;
@@ -14,14 +15,16 @@ use Symfony\Component\Serializer\SerializerInterface;
 class RecetteController extends AbstractController
 {
     private RecetteRepository $recetteRepository;
+    private EntityManagerInterface $entityManager;
     private SerializerInterface $serializer;
-    public function __construct(RecetteRepository $recetteRepository, SerializerInterface $serializer)
+    public function __construct(RecetteRepository $recetteRepository, SerializerInterface $serializer, EntityManagerInterface $entityManager)
     {
         $this->recetteRepository = $recetteRepository;
         $this->serializer = $serializer;
+        $this->entityManager = $entityManager;
     }
 
-    #[Route('/recette', name: 'app_recette')]
+    #[Route('/recette', name: 'app_recette', methods: 'GET')]
     public function index(): JsonResponse
     {
         return new JsonResponse(
@@ -31,5 +34,36 @@ class RecetteController extends AbstractController
             [],
             true
         );
+    }
+
+    #[Route('/recette/{id}', name: 'app_one_recette', methods: 'GET')]
+    public function findOneRecette(int $id): JsonResponse
+    {
+        $recette = $this->recetteRepository->findOneBy(['id' => $id]);
+
+        if(null === $recette)
+            return new JsonResponse($this->serializer->serialize("Recette doesn't exist",'json'),Response::HTTP_BAD_REQUEST, [], true);
+
+        return new JsonResponse(
+            $this->serializer->serialize($recette,
+                'json', ["groups" => "getRecette"]),
+            Response::HTTP_OK,
+            [],
+            true
+        );
+    }
+
+    #[Route('/recette/{id}', name: 'app_one_recette', methods: 'DELETE')]
+    public function deleteRecette(int $id): JsonResponse
+    {
+        $recette = $this->recetteRepository->findOneBy(['id' => $id]);
+//        dd($recette);
+        if(null === $recette)
+            return new JsonResponse($this->serializer->serialize("Recette doesn't exist",'json'),Response::HTTP_BAD_REQUEST, [], true);
+
+        $this->recetteRepository->remove($recette);
+        $this->entityManager->flush();
+        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+
     }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -46,11 +46,11 @@ class AppFixtures extends Fixture
     {
         $user->setEmail('boddaert.gauthier@gmail.com');
         $user->setPassword($this->userPasswordHasher->hashPassword($user, "coucou"));
-        $user->setRoles(['user']);
+        $user->setRoles(['ROLE_ADMIN']);
 
         $admin->setEmail('gboddaert@insitaction.com');
         $admin->setPassword($this->userPasswordHasher->hashPassword($admin, "coucou"));
-        $admin->setRoles(['admin']);
+        $admin->setRoles(['ROLE_ADMIN']);
 
         $manager->persist($user);
         $manager->persist($admin);
