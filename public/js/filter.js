a926f39b3e8215c6c342ec61377a9245f2fd558c
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2023-01-22 10:55:54 +0100

    WIP filter in js

--- public/js/filter.js
+++ public/js/filter.js
@@ -1,3 +1,14 @@
-$(document).ready(function() {
-    alert('coucou');
-  });
+//open filter
+const openFilter = document.getElementsByClassName("filterOpen");
+
+openFilter[0].addEventListener("click", function(){
+    document.getElementById("overlay").style.display = "flex";
+});
+
+//close filter
+const closeIcon = document.getElementsByClassName("closefitlreRecipe");
+
+closeIcon[0].addEventListener("click", function(){
+    document.getElementById("overlay").style.display = "none";
+});
+
