5a93191e30d890aa0b8e2b89c46d3b58372b6ba0
Gauthier Boddaert <boddaert.gauthier@gmail.com>
2023-03-10 19:54:30 +0100

    remove somes files useless for the new version of my app

--- src/Service/RecipeManager.php
+++ src/Service/RecipeManager.php
@@ -28,15 +28,15 @@ class RecipeManager implements RecipeManagerInterface
 
     public function __construct
     (
-        RecipeRepository $recipeRepository,
-        ImageManagerInterface $imageManager,
-        RepositoryInterface $repository,
-        EntityManagerInterface $entityManager,
-        UserRepository $userRepository,
+        RecipeRepository           $recipeRepository,
+        ImageManagerInterface      $imageManager,
+        RepositoryInterface        $repository,
+        EntityManagerInterface     $entityManager,
+        UserRepository             $userRepository,
         IngredientManagerInterface $ingredientManager,
-        CategoryManagerInterface $categoryManager,
+        CategoryManagerInterface   $categoryManager,
         RecipeStepManagerInterface $recipeStepManager,
-        string $image_directory,
+        string                     $image_directory,
 
     )
     {
@@ -51,49 +51,6 @@ class RecipeManager implements RecipeManagerInterface
         $this->recipeStepManager = $recipeStepManager;
     }
 
-    public function setNewCategory(Category $category , Recipe $recipe) : void
-    {
-        $recipe->removeCategory($recipe->getCategory()[0]);
-        $recipe->addCategory($category);
-    }
-
-    public function createNewRecipe(Recipe $recipe,User $user, FormInterface $form ) : bool
-    {
-        $like = new Like();
-        $like->setUser($user);
-        $like->setIsLike(false);
-        $like->setRecipe($recipe);
-
-        $favoris = new Favoris();
-        $favoris->setIsFavoris(false);
-        $favoris->addUser($user);
-        $favoris->addRecette($recipe);
-
-        //set the user here like this I don't need to show it in the form
-        $recipe->setUsers($user);
-
-        //allow to move image in directory of project uploads/image_recipe
-        $this->imageManager->downloadImage($form, $recipe,$this->recipeRepository);
-
-        $this->entityManager->persist($recipe);
-        $this->entityManager->persist($like);
-        $this->entityManager->persist($favoris);
-
-        $this->entityManager->flush();
-
-        return true;
-    }
-
-
-    public function paginatorForTenRecipe(Request $request,array $array) : SlidingPagination
-    {
-        return $this->paginator->paginate(
-            $array,
-            $request->query->getInt('page', 1),
-            $request->query->getInt('limit', 9)
-        );
-    }
-
     public function createRecipeFromRequest(array $request): void
     {
         $recipe = new Recipe();
@@ -105,7 +62,7 @@ class RecipeManager implements RecipeManagerInterface
         $recipe->setGlucide($request['glucide']);
         $recipe->setProteine($request['proteine']);
         $recipe->setLipide($request['lipide']);
-        $this->ingredientManager->addIngredientsInRecipe($recipe,$request['ingredients'] );
+        $this->ingredientManager->addIngredientsInRecipe($recipe, $request['ingredients']);
         $this->categoryManager->addCategoryInRecipe($recipe, $request['categories']);
         $this->recipeStepManager->addRecipeStepsInRecipe($recipe, $request['steps']);
         $recipe->setCreationTime($request['creationTime']);
@@ -118,7 +75,7 @@ class RecipeManager implements RecipeManagerInterface
     {
         $recipe = $this->recipeRepository->findOneBy(['id' => $id]);
 
-        (!empty($recipes['name'])) ? $recipe->setName($recipes['name']) : '' ;
+        (!empty($recipes['name'])) ? $recipe->setName($recipes['name']) : '';
         (!empty($recipes['description'])) ? $recipe->setDescriptions($recipes['description']) : '';
         (!empty($recipes['number_of_persons'])) ? $recipe->setNumberOfPersons($recipes['number_of_persons']) : '';
         (!empty($recipes['creationTime'])) ? $recipe->setCreationTime($recipes['creationTime']) : '';
@@ -126,7 +83,6 @@ class RecipeManager implements RecipeManagerInterface
         (!empty($recipes['updatedAt'])) ? $recipe->setUpdatedAt(new \DateTime()) : '';
 
         //TODO update ingredients and category and recipeStep
-
 
         return $recipe;
     }
--- src/Service/UserManager.php
+++ src/Service/UserManager.php
@@ -6,9 +6,5 @@ use App\Entity\User;
 
 class UserManager
 {
-    public function getAge(User $user) : int
-    {
-        $date = new \DateTime();
-        return $date->diff($user->getDateOfBirth())->y;
-    }
+
 }
